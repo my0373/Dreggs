60caded655bdf197737752d5429fd02f1e5b23d0
Matt York <my0373@gmail.com>
2023-11-03 19:24:42 +0000

    Created some DB migrations

--- store/models.py
+++ store/models.py
@@ -15,10 +15,11 @@ class Collection(models.Model):
 class Product(models.Model): 
     title = models.CharField(verbose_name="product title",
                              max_length=255,)
+    slug = models.SlugField(default='-')
     
     description = models.TextField()
     
-    price = models.DecimalField(max_digits=6,
+    unit_price = models.DecimalField(max_digits=6,
                                 decimal_places=2)
     
     inventory = models.IntegerField()
@@ -61,6 +62,7 @@ class Customer(models.Model):
                                   max_length=1,
                                   choices=MEMBERSHIP_CHOICES,
                                   default=MEMBERSHIP_BRONZE)
+    
     
 class Order(models.Model):
     PAYMENT_PENDING = "p"
